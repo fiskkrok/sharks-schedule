dd76b0620ea544cf106e2fcfefd5349aef8ae378
Anthony Liljegren <camerons@live.se>
2024-11-01 21:10:29 +0100

    dawd

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api/v1': {
+        target: 'https://api-web.nhle.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        followRedirects: true,
+        secure: false
+      },
+      '/api/logos': {
+        target: 'https://www-league.nhlstatic.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/logos/, ''),
+        secure: false
+      }
+    }
+  }
+})
